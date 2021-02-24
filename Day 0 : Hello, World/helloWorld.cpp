1ad2744df5e2078d4b682e4a93387f8a3f1cf420
mpscoder <67371648+mpscoder@users.noreply.github.com>
2021-02-24 11:21:36 +0530

    Create helloWorld.cpp

---
+++ Day 0 : Hello, World/helloWorld.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define     ll          long long
+#define     pb          push_back
+
+#define     endl          "\n"
+#define     srt(a)      sort(a.begin(), a.end())
+#define     rsrt(a)     sort(a.rbegin(), a.rend())
+#define     rev(a)      reverse(a.begin(), a.end())
+#define     readme      ios_base::sync_with_stdio(false);cin.tie(0),cout.tie(0)
+
+
+int main() {
+    
+    readme;
+  
+  
+    string input_string;
+    getline(cin, input_string);
+    
+    cout<< "Hello, World." <<"\n";
+    cout<<input_string <<"\n";
+ 
+    return 0;
+}
