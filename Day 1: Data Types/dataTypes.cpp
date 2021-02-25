893d79bc252435bd3e8277ba2f0b835ab9cb5bb7
mpscoder <67371648+mpscoder@users.noreply.github.com>
2021-02-25 09:35:51 +0530

    Create dataTypes.cpp

---
+++ Day 1: Data Types/dataTypes.cpp
@@ -0,0 +1,36 @@
+/*********************************************************************************************************
+                                  Author : MAMATA SHEE ( @mpscoder )
+
+**********************************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+
+    
+    // Declare second integer, double, and String variables.
+    int n; double dou; string st;
+    
+    // Read and save an integer, double, and String to your variables.
+    cin>> n >> dou;
+    cin.ignore();
+    getline(cin, st);
+    
+    // Print the sum of both integer variables on a new line.
+    cout << i+n << endl;
+    
+    // Print the sum of the double variables on a new line.
+    cout << fixed << setprecision(1) << (d+ dou) <<endl;
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cout << s+st ;
+
+    return 0;
