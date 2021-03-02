dc6774255fe0c5e009b1c13aab940f77c5f14058
mpscoder <67371648+mpscoder@users.noreply.github.com>
2021-03-02 09:27:56 +0530

    Create letsReview.cpp

---
+++ Day 6: Let's Review/letsReview.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************************************************
+
+                           Day 6: Let's Review | HackerRank #30days-of-code
+                            
+                                    Author : MAMATA SHEE
+                                       ( @mpscoder )
+                                       
+*******************************************************************************************************************/
+
+
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+
+
+int main() {
+    
+    int t; cin>>t;
+    while(t--){
+        cin.ignore();
+        string s;
+        cin >> s;
+        
+        for(int i = 0; i < s.length(); i += 2){
+            cout << s[i];
+        }
+        cout << " ";
+        for(int i = 1; i < s.length(); i += 2){
+            cout << s[i];
+        }
+        
+        cout << endl;
+    }
+    
+    return 0;
+}
