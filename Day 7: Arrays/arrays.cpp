0d6a4de80172c2f3c4563a2f7060bfbfb01f61bd
mpscoder <67371648+mpscoder@users.noreply.github.com>
2021-03-03 19:13:44 +0530

    Create arrays.cpp

---
+++ Day 7: Arrays/arrays.cpp
@@ -0,0 +1,29 @@
+/****************************************************************************************************
+
+                                   Day 7: Arrays | HackerRank #30days-of-code
+                                   
+                                         Author : MAMATA SHEE
+                                            ( @mpscoder )
+
+******************************************************************************************************/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    
+    int a[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    
+    for(int i = n - 1; i >= 0; i--){
+        cout << a[i] << " ";
+    }
+    
+        return 0;
+}
