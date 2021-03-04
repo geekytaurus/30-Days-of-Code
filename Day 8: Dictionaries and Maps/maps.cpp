f2760713f64e6ef9ebe254d14c0fdf5ec92a8153
mpscoder <67371648+mpscoder@users.noreply.github.com>
2021-03-04 14:44:00 +0530

    Create maps.cpp

---
+++ Day 8: Dictionaries and Maps/maps.cpp
@@ -0,0 +1,44 @@
+/*******************************************************************************************************************
+
+                         Day 8: Dictionaries and Maps | HackerRank #30daysOfCode
+                                       
+                                      Author : MAMATA SHEE
+                                         ( @mpscoder )
+                                         
+********************************************************************************************************************/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;
+    
+    map<string, int> nameNum;
+    map<string, int>::iterator it;
+    int j;
+    string s, s1;
+    
+   //-----------------insertig n numbers of key and value pair in the map------------------------
+    for(int i = 1; i <= n; i++){
+        cin >> s >> j;
+        nameNum.insert(pair<string, int>(s, j));
+    }
+    
+  
+  //----------taking unknown number of quaries and then find in the map--------------------------
+    while(cin >> s1){
+        if(nameNum.find(s1) == nameNum.end()){
+            cout << "Not found" << endl;
+        }
+        else{
+            it = nameNum.find(s1);
+            cout << it->first << "=" << it->second << endl;
+        }
+    }
+    return 0;
+}
+
+                                      
